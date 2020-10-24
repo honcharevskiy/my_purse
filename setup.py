f14eb9881507bae115b17ef73478d45435bf664c
honchar <goncharevskiyy@gmail.com>
2020-10-24 17:01:33 +0300

    Use pip instead of Pipenv.

---
+++ setup.py
@@ -0,0 +1,36 @@
+import setuptools
+
+setuptools.setup(
+    name='my_purse',
+    version='0.0.1',
+    author='Honchar',
+    description=(
+        'Store bank transactions into purse.'
+    ),
+    url='https://github.com/honcharevskiy/my_purse',
+    packages=setuptools.find_packages(
+        exclude=[
+            'tests',
+            'tests.*',
+        ],
+    ),
+    install_requires=[
+        # API
+        'mangum',
+        'fastapi',
+        'pydantic',
+        'uvicorn',
+
+        # CLI
+        'fire',
+
+        # Monitoring
+        'sentry_sdk',
+
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: Other/Proprietary License',
+        'Operating System :: OS Independent',
+    ],
+)
